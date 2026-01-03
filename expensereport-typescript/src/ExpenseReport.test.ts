6ff20038471398f8268d112ceaf4e0dc0d3c96bd
AhmedHdeawy <ahmedhdeawy@gmail.com>
2026-01-03 09:47:06 +0200

    refactor: Update test cases in ExpenseReport to use consistent date formatting

--- expensereport-typescript/src/ExpenseReport.test.ts
+++ expensereport-typescript/src/ExpenseReport.test.ts
@@ -1,18 +1,19 @@
 import { printHelloWorld, sumTwoValues, Expense, ExpenseReport } from './ExpenseReport'
 
 describe(`ExpenseReport`, () => {
-    it(`should keep its original behavior`, () => {
-        let interceptedOutput = ""
-        jest.spyOn(process.stdout, "write").mockImplementation((output: string): boolean => {
-            interceptedOutput += output
-            return true;
-        })
-
-      new ExpenseReport([new Expense('dinner', 5001)]).printReport()
-        expect(interceptedOutput).toEqual(
-          `Expenses: ${new Date().toISOString().substr(0, 10)}\nDinner\t5001\tX\nMeal Expenses: 5001\nTotal Expenses: 5001\n`,
-        )
+  const todayDate = new Date().toISOString().substr(0, 10)
+  it(`should keep its original behavior`, () => {
+    let interceptedOutput = ''
+    jest.spyOn(process.stdout, 'write').mockImplementation((output: string): boolean => {
+      interceptedOutput += output
+      return true
     })
+
+    new ExpenseReport([new Expense('dinner', 5001)]).printReport()
+    expect(interceptedOutput).toEqual(
+      `Expenses: ${todayDate}\nDinner\t5001\tX\nMeal Expenses: 5001\nTotal Expenses: 5001\n`,
+    )
+  })
 
   it(`should keep its original behavior for breakfast`, () => {
     let interceptedOutput = ''
@@ -23,10 +24,9 @@ describe(`ExpenseReport`, () => {
 
     new ExpenseReport([new Expense('breakfast', 1003)]).printReport()
     expect(interceptedOutput).toEqual(
-      `Expenses: ${new Date().toISOString().substr(0, 10)}\nBreakfast\t1003\tX\nMeal Expenses: 1003\nTotal Expenses: 1003\n`,
+      `Expenses: ${todayDate}\nBreakfast\t1003\tX\nMeal Expenses: 1003\nTotal Expenses: 1003\n`,
     )
   })
-
 
   it(`should keep its original behavior for car-rental`, () => {
     let interceptedOutput = ''
@@ -37,7 +37,7 @@ describe(`ExpenseReport`, () => {
 
     new ExpenseReport([new Expense('car-rental', 3000)]).printReport()
     expect(interceptedOutput).toEqual(
-      `Expenses: ${new Date().toISOString().substr(0, 10)}\nCar Rental\t3000\t \nMeal Expenses: 0\nTotal Expenses: 3000\n`,
+      `Expenses: ${todayDate}\nCar Rental\t3000\t \nMeal Expenses: 0\nTotal Expenses: 3000\n`,
     )
   })
 
@@ -56,7 +56,7 @@ describe(`ExpenseReport`, () => {
       new Expense('dinner', 6000),
     ]).printReport()
     expect(interceptedOutput).toEqual(
-      `Expenses: ${new Date().toISOString().substr(0, 10)}\nDinner\t4000\t \nBreakfast\t800\t \nCar Rental\t2000\t \nDinner\t6000\tX\nMeal Expenses: 10800\nTotal Expenses: 12800\n`,
+      `Expenses: ${todayDate}\nDinner\t4000\t \nBreakfast\t800\t \nCar Rental\t2000\t \nDinner\t6000\tX\nMeal Expenses: 10800\nTotal Expenses: 12800\n`,
     )
   })
 
@@ -70,37 +70,38 @@ describe(`ExpenseReport`, () => {
 
     new ExpenseReport([]).printReport()
     expect(interceptedOutput).toEqual(
-      `Expenses: ${new Date().toISOString().substr(0, 10)}\nMeal Expenses: 0\nTotal Expenses: 0\n`,
+      `Expenses: ${todayDate}\nMeal Expenses: 0\nTotal Expenses: 0\n`,
     )
   })
 })
 
 describe(`given I have this test suite`, () => {
-    it(`should always output Hello, World!`, () => {
-        //given
-        let actualOutputData = ""
-        jest.spyOn(process.stdout, "write").mockImplementation((data: string): boolean => {
-            actualOutputData += data
-            return true
-        })
-        const expectedOutputData = "Hello, World!\n"
-
-        // when
-        printHelloWorld()
-
-        // then
-        expect(actualOutputData).toEqual(expectedOutputData)
+  it(`should always output Hello, World!`, () => {
+    //given
+    let actualOutputData = ''
+    jest.spyOn(process.stdout, 'write').mockImplementation((data: string): boolean => {
+      actualOutputData += data
+      return true
     })
+    const expectedOutputData = 'Hello, World!\n'
 
-    it(`should always do the correct sum`, () => {
-        // given
-        const a = 2, b = 3
-        const expectedValue = 5
+    // when
+    printHelloWorld()
 
-        // when
-        const actualValue = sumTwoValues(a, b)
+    // then
+    expect(actualOutputData).toEqual(expectedOutputData)
+  })
 
-        // then
-        expect(actualValue).toEqual(expectedValue)
-    })
+  it(`should always do the correct sum`, () => {
+    // given
+    const a = 2,
+      b = 3
+    const expectedValue = 5
+
+    // when
+    const actualValue = sumTwoValues(a, b)
+
+    // then
+    expect(actualValue).toEqual(expectedValue)
+  })
 })
